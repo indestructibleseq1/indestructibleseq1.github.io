fd2e6edfb1e44e6b912bc95ba112af9e01a87203
indestructibleseq1 <indestructibleseq1@gmail.com>
2025-10-09 23:49:07 -0600

    Delete configura.js

--- configura.js
+++
@@ -1,9 +0,0 @@
-/** Barra de navegación. */
-import "../lib/js/custom/md-menu-button.js"
-import "../lib/js/custom/md-options-menu.js"
-import "../lib/js/custom/md-overflow-button.js"
-import "../lib/js/custom/md-overflow-menu.js"
-import "../lib/js/custom/md-select-menu.js"
-import "../lib/js/custom/md-top-app-bar.js"
-import "../lib/js/custom/md-slider-field.js"
-
